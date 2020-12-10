4454f952b4523e06094b74427b491784f784c4ee
rubenvelasquez <rubenvelasq93@gmail.com>
2020-12-09 20:18:04 -0400

    v2

--- js/myScript.js
+++ js/myScript.js
@@ -1,2 +1,12 @@
-document.getElementById("demo").innerHTML =
-"page location is " + window.location.protocol;
+function myFunction(){
+    var txt;
+    var person = prompt("Introduzca su nombre :",
+    "ruben velasque");
+    if(person == null || person ==""){
+        txt = "El usuario caneclo el mensaje.";
+    }else{
+        txt = "hola" + person +" como esta hoy ";
+    }
+
+    document.getElementById("demo").innerHTML = txt;
+}
